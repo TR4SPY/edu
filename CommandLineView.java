4f6e3c6323aaa0190b55363db169f3c1550ae5ce
Karol Machajewski <33313091+TR4SPY@users.noreply.github.com>
2024-06-05 17:22:05 +0100

    Delete CommandLineView.java
    File moved to MSc/POP_Connect_Four_Java/

--- CommandLineView.java
+++
@@ -1,9 +0,0 @@
-public class CommandLineView implements View {
-    public static final String ANSI_CYAN = "\u001B[36m";
-    public static final String ANSI_RESET = "\u001B[0m";
-    public void display(String s) {
-
-        System.out.println(ANSI_CYAN + s + ANSI_RESET);
-    }
-
-}
